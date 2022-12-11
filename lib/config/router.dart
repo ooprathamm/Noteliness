1822a769733d369b4e4c8893fe09d751a3a4e2f9
rajputchauhan1 <prathamchauhan2001@gmail.com>
2022-12-11 23:07:55 +0530

    Added router (go_router)

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,5 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
-class AppRouter {
+import '../screens/wall_screen.dart';
 
+class AppRouter {
+  // all the route paths. So that we can access them easily  across the app
+  static const root = '/wall_screen';
+
+
+  /// private static methods that are accessible only in this class and not from outside
+  static Widget _homePageRouteBuilder(BuildContext context, GoRouterState state) => WallScreen();
+
+  /// use this in [MaterialApp.router]
+  static final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(path: root, builder: _homePageRouteBuilder),
+    ],
+  );
+
+  static GoRouter get router => _router;
 }
