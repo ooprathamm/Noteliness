97117f5135bcc2226428ed7805324ab55c935f27
rajputchauhan1 <prathamchauhan2001@gmail.com>
2022-12-11 21:55:09 +0530

    added widgets

--- lib/constants/strings.dart
+++
@@ -1,2 +0,0 @@
-const wallScreen = '/';
-const chooseScreen = '/choose_screen';
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 
 import '../screens/splash_screen.dart';
-import '../router/router.dart';
 
-void main() => runApp(_Noteliness(
-  router: Router(),
-));
+void main() => runApp(_Noteliness());
 
 class _Noteliness extends StatelessWidget {
-  final Router router;
-  const _Noteliness({super.key,required this.router});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen()
+      home: SplashScreen(),
     );
   }
 }
+
 
